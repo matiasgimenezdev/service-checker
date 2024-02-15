541994ca51efcbe50bfb8af4edc437a32a8c6fea
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-15 20:38:26 -0300

    add: cron service & node-cron dependency

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,10 @@
+import { CronService } from './cron/cron-service';
+
 export class Server {
 	public static start() {
-		console.log('Server started...');
+		CronService.createJob('*/5 * * * * *', () => {
+			const date = new Date();
+			console.log('Running job at', date);
+		});
 	}
 }
