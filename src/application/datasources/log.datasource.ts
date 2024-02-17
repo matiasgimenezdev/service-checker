f93f95a5179a3bbcd59eb84f7ddaa62a033adbac
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-17 16:43:43 -0300

    chore: rename files

--- src/application/datasources/log.datasource.ts
+++ src/application/datasources/log.datasource.ts
@@ -1,4 +1,4 @@
-import { LogEntity, LogSeverityLevel } from '../../domain/entities/logEntity';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
 
 export abstract class LogDatasource {
 	abstract saveLog(log: LogEntity): Promise<void>;
