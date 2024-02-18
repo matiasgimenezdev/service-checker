8e6e33e95d9c50bc45f80ee7813781b79152c6ae
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-17 21:32:08 -0300

    feat: instanciate dependencies

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
 import { CheckService } from '../domain/usecases/checks/check-service';
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource.';
+import { LogRepository } from '../infrastructure/repositories/log.repository';
 import { CronService } from './cron/cron-service';
+
+// Instanciate the dependencies
+const fileSystemDatasource = new FileSystemDatasource();
+const fileSystemLogRepository = new LogRepository(fileSystemDatasource);
 
 export class Server {
 	public static start() {
@@ -8,7 +14,8 @@ export class Server {
 
 			new CheckService(
 				() => console.log(`Service ${url} is up!`),
-				(error) => console.log(error)
+				(error) => console.log(error),
+				fileSystemLogRepository
 			).execute(url);
 		});
 	}
