641987e803d95347ede70ea42965366863754f3e
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-18 16:39:56 -0300

    feat: add env variables

---
+++ src/config/plugins/env.plugin.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config';
+import * as env from 'env-var';
+
+export const envs = {
+	MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
+	MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+	PRODUCTION: env.get('PRODUCTION').required().asBool(),
+};
