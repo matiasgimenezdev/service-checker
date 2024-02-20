83b741e9ed75c4a24a3806d5aea0e4dc60c41373
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-20 19:23:09 -0300

    fix: remove console.log()

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
-import { envs as env } from './config/plugins/env.plugin';
 import { Server } from './presentation/server';
 
 function main() {
 	Server.start();
-
-	//const { MAILER_EMAIL, MAILER_SECRET_KEY, PRODUCTION } = env;
-	// console.log({
-	// 	MAILER_EMAIL,
-	// 	MAILER_SECRET_KEY,
-	// 	PRODUCTION,
-	// });
 }
 
 (async () => {
