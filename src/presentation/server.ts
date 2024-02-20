44e2bf159703f69e03319064694e471dbd9aa74e
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-20 12:22:32 -0300

    refactor: rename file

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,6 @@
 import { CheckService } from '../domain/usecases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
-import { CronService } from './cron/cron-service';
+import { CronService } from './cron/cron-job.service';
 import { EmailService } from './mailer/email.service';
 import { LogRepository } from '../infrastructure/repositories/log.repository';
 
@@ -10,14 +10,14 @@ const fileSystemLogRepository = new LogRepository(fileSystemDatasource);
 
 export class Server {
 	public static async start() {
-		// CronService.createJob('*/5 * * * * *', () => {
-		// 	const url = 'https://google.com';
-		// 	new CheckService(
-		// 		fileSystemLogRepository,
-		// 		() => console.log(`Service ${url} is up!`),
-		// 		(error) => console.log(error)
-		// 	).execute(url);
-		// });
+		CronService.createJob('*/5 * * * * *', () => {
+			const url = 'https://google.com';
+			new CheckService(
+				fileSystemLogRepository,
+				() => console.log(`Service ${url} is up!`),
+				(error) => console.log(error)
+			).execute(url);
+		});
 		//
 		// const emailService = new EmailService(fileSystemLogRepository);
 		// await emailService.sendEmailWithFileSystemLogs(
