ed4339dd9c646029c8db33e2fff318f7b0231152
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-21 18:54:04 -0300

    feat: update log entity

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -40,4 +40,10 @@ export class LogEntity {
 
 		return log;
 	}
+
+	public static fromObject(object: { [key: string]: any }): LogEntity {
+		const { message, level, createdAt, origin } = object;
+		const log = new LogEntity({ message, level, createdAt, origin });
+		return log;
+	}
 }
