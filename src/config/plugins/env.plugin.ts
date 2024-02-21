043d34ed406995242eb9d93838a624eb2574fcd3
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2024-02-21 17:40:37 -0300

    feat: add mongo environments

--- src/config/plugins/env.plugin.ts
+++ src/config/plugins/env.plugin.ts
@@ -5,5 +5,11 @@ export const envs = {
 	MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
 	MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
 	MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
+
 	PRODUCTION: env.get('PRODUCTION').required().asBool(),
+
+	MONGO_URL: env.get('MONGO_URL').required().asString(),
+	MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+	MONGO_DB_USER: env.get('MONGO_DB_USER').required().asString(),
+	MONGO_DB_PASSWORD: env.get('MONGO_DB_PASSWORD').required().asString(),
 };
